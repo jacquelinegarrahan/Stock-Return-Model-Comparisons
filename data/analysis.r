0fb93f8467447235a52487b3e330173648fc9434
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-06-14 14:26:49 -0400

    Remove unused plot and data prep

--- data/analysis.r
+++
@@ -1,9 +0,0 @@
-
-close1.data <- read.csv(file="close_1_returns.csv", header=TRUE, sep=",")
-close5.data <- read.csv(file="close_5_returns.csv", header=TRUE, sep=",")
-close20.data <- read.csv(file="close_20_returns.csv", header=TRUE, sep=",")
-
-library(psych)
-describe(close1.data)
-describe(close5.data)
-describe(close20.data)
